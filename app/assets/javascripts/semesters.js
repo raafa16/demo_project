49e329dd94fd1817c3fc2545dcc3e6b3611d12e5
raafa16 <raafa16@hotmail.com>
2016-11-01 12:09:24 +0600

    Minor changes

---
+++ app/assets/javascripts/semesters.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    dataConfirmModal.setDefaults({
+        title: 'You are about to delete this semester.',
+        commit: 'Yes',
+        cancel: 'No'
+    });
+});
+
