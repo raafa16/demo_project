1c410820a5912c07b71a68ce8bdd0e74f72a7ecd
raafa16 <raafa16@hotmail.com>
2016-11-10 15:11:27 +0600

    checked already registered course

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def check_if_user_registered_this_course(current_user, course_id, semester_id)
+  @course_id = course_id
+  @semester_id = semester_id
+    if current_user.courses_users.exists?(:user_id=> current_user.id, :course_id=> @course_id, :semester_id=>@semester_id) == true
+      return true
+    else
+      return false
+    end
+  end
 end
